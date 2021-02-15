7dd5cfe08af3ea0becf66e7e79175a6260249801
himanshudas75 <himanshudas75@gmail.com>
2021-02-15 21:10:03 +0530

    Changes were made

--- FileType.py
+++ FileType.py
@@ -3,7 +3,5 @@ import re
 
 def identify(inploc):
 	p1=subprocess.run(['file',inploc],capture_output=True,text=True)
-	p2=re.findall(r' {1}\w+ {1}',p1.stdout)
-	p2=p2[0]
-	p2=p2[1:-1]
+	p2=re.findall(r' {1}\w+ {1}',p1.stdout)[0][1:-1]
 	return p2
