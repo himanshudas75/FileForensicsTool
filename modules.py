de37c7167d884b0bce3be5fc73b974c362dee367
himanshudas75 <himanshudas75@gmail.com>
2021-02-16 23:29:35 +0530

    Changes were made

--- modules.py
+++ modules.py
@@ -4,10 +4,13 @@ import re
 def zsteg(location):
 	#PNG
 	data = subprocess.run(['zsteg','-a',location],text=True,capture_output=True)
-	data = re.findall(r'text: ".*"',data.stdout)
-	for i in range(len(data)):
-		data[i]=data[i][7:-1]
-	print(data)
+	data=data.stdout
+	text = re.findall(r'text: ".*"',data)
+	ctf=re.findall(r'\w+[ctf]{.+}|\w+[CTF]{.+}',data)
+	for i in range(len(text)):
+		text[i]=text[i][7:-1]
+	print(text)
+	print(ctf)
 
 def strings(location):
 	#PNG, JPEG
