bda54b3d0c9e29eda085cd626a9252e26b971ae5
himanshudas75 <himanshudas75@gmail.com>
2021-02-16 12:07:28 +0530

    Changes were made

--- png.py
+++ png.py
@@ -5,9 +5,10 @@ def strings(location):
 	data = subprocess.run(['strings',location],text=True,capture_output=True)
 
 def zsteg(location):
-	data = subprocess.run(['zsteg',location],text=True,capture_output=True)
+	data = subprocess.run(['zsteg','-a',location],text=True,capture_output=True)
 	data = re.findall(r'text: ".*"',data.stdout)
 	for i in range(len(data)):
 		data[i]=data[i][7:-1]
+	print(data)
 
 	
